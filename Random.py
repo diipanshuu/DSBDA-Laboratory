e2a9dd159b66682e168b72a183e6067af3dcc335
diipanshuu <103811070+diipanshuu@users.noreply.github.com>
2022-07-13 23:39:37 +0530

    Update for Instagram Reach Analysis!

--- Random.py
+++ Random.py
@@ -129,3 +129,15 @@ def PredictTrials(X, y, fitter, data):
 
     # Display price range
     print("\nRange in prices: ${:,.2f}".format(max(prices) - min(prices)))
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import PassiveAggressiveRegressor
+
+data = pd.read_csv("Instagram.csv", encoding = 'latin1')
+print(data.head())
